b78e40d7854db19383eef598950f19469cfa0471
shivam24121 <Shivam050905@gmail.com>
2025-09-11 23:32:29 +0530

    Create Solution.java

---
+++ Linked List Insertion At End/Solution.java
@@ -0,0 +1,25 @@
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        if(head==null){
+            return new Node(x);
+        }
+        Node temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=new Node(x);
+        return head;
+    }
+}
