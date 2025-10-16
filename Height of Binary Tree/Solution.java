0c2b6b296445cd44a4bd8465c7d7e35fa719db8c
shivam24121 <Shivam050905@gmail.com>
2025-10-16 22:43:52 +0530

    Create Solution.java

---
+++ Height of Binary Tree/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int height(Node root) {
+        if(root==null)return -1;
+        
+        int left=height(root.left);
+        int right=height(root.right);
+        
+        return 1+Math.max(left,right);
+    }
+}
