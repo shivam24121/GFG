ddd511106cdd750bc98d9aa825c8e0ac277c9cde
shivam24121 <Shivam050905@gmail.com>
2025-10-17 17:19:05 +0530

    Create Solution.java

---
+++ Balanced Tree Check/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isBalanced(Node root) {
+        return dfs(root)!=-1;
+    }
+    public int dfs(Node root){
+        if(root==null)return 0;
+        
+        int left=dfs(root.left);
+        if(left==-1)return -1;
+        
+        int right=dfs(root.right);
+        if(right==-1)return -1;
+        
+        if(Math.abs(left-right)>=2)return -1;
+        return 1+Math.max(left,right);
+    }
+}
