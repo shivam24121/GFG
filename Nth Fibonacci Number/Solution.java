c5fa7f24d82cffcee762bad1e0280d796536239c
shivam24121 <Shivam050905@gmail.com>
2025-10-21 19:20:25 +0530

    Create Solution.java

---
+++ Nth Fibonacci Number/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int nthFibonacci(int n) {
+        if(n<=1)return n;
+        int prev2=0;
+        int prev1=1;
+        for(int i=2;i<=n;i++){
+            int curr=prev2+prev1;
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
