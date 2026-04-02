a41d1cbf7e11dcb05664983094091892e0ac770b
shivam24121 <Shivam050905@gmail.com>
2026-04-02 22:35:37 +0530

    Create Main.java

---
+++ Painting the Fence/Main.java
@@ -0,0 +1,19 @@
+class Solution {
+    int countWays(int n, int k) {
+        
+        if(n==1)return k;
+        
+        int same=k;
+        int diff=k*(k-1);
+        
+        for(int i=3;i<=n;i++){
+            
+            int newSame=diff;
+            int newDiff=(same+diff)*(k-1);
+            
+            same=newSame;
+            diff=newDiff;
+        }
+        return same+diff;
+    }
+}
